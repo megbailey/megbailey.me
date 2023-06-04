db4b8032389494257fd7d446c7ac6123b82add15
mbailey <meganbailey@sandiego.edu>
2023-06-03 17:21:58 -0700

    styling posts and menu properly

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from "remark-gfm";
 
+import '../../assets/styles/post.css'
 //const id = new URLSearchParams(window.location.href).search.get('id');
 const postsConfig = require('../../assets/posts.json');
 const id = 0
@@ -22,7 +24,12 @@ const Post = ( props ) => {
             .catch( error => console.log(error) )
     }, [type]);
     
-    return <ReactMarkdown>{md}</ReactMarkdown>
+    return (
+        <ReactMarkdown 
+            className='reactmd'
+            remarkPlugins={[remarkGfm]}
+        >{md}</ReactMarkdown>
+    )
 
 }
 
